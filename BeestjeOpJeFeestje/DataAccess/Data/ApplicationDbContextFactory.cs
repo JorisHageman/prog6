997f24854b76cf9c443692a7e561928bb5ae9f8b
Joris Hageman <jorishageman04@gmail.com>
2024-04-09 12:46:37 +0200

    Update ApplicationDbContextFactory.cs

--- BeestjeOpJeFeestje/DataAccess/Data/ApplicationDbContextFactory.cs
+++ BeestjeOpJeFeestje/DataAccess/Data/ApplicationDbContextFactory.cs
@@ -5,7 +5,7 @@ using Models;
 public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext> {
     public ApplicationDbContext CreateDbContext(string[] args) {
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-        optionsBuilder.UseSqlServer("Server=localhost;Database=BeestjeOpJeFeestjeDb;Trusted_Connection=True;TrustServerCertificate=true;");
+        optionsBuilder.UseSqlServer("Server=tcp:beestjeopjefeestjedb.database.windows.net,1433;Initial Catalog=BeestjeOpJeFeestjeDB;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication='Active Directory Default';");
 
         return new ApplicationDbContext(optionsBuilder.Options);
     }
